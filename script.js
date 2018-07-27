6f2d88e9fce95885b8fe942374a77c2f8454dfc8
DePaul <CONF0209@depaul.edu>
2018-07-27 15:38:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+	var numbers = [45, 54, 90, 34562222221, 78, 47,34];
+	numbers.sort();
+	numbers.reverse();
+	console.log(numbers);
+
+	for (var j=0; j<10; j++) {
+		console.log(j*2);
+	}
+	
+	var i = 0;
+	while (i<=10) {
+		i+=1;
+	}
+	
+	console.log(i*10);
+	
+	
+});
